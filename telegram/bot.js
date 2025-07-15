9e46f035226b026f7debfac599a17cb6856904da
kipa4ka <bohdankrav98@gmail.com>
2025-07-15 20:47:21 +0300

    update

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '7566012207:AAHweYz8ujJ17gz_-2QcqIghdRiHDv72iLQ';
+const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  const url = 'https://your-domain.com'; // Або localhost:3000 на тесті
-  bot.sendMessage(chatId, 'Натисни кнопку, щоб запустити гру', {
-    reply_markup: {
-      inline_keyboard: [[
-        { text: 'Запустити гру', web_app: { url } }
-      ]]
-    }
-  });
+    const chatId = msg.chat.id;
+    const url = 'https://dainty-dodol-f6a881.netlify.app/index.html';
+    bot.sendMessage(chatId, 'Натисни кнопку, щоб запустити гру', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Запустити гру', web_app: { url } }]
+            ]
+        }
+    });
 });
